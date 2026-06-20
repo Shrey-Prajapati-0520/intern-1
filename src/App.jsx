b3860ec05557718feb62e0cbc8ef7b570c2801a6
Meet-Prajapati-MP <pmeet9174@gmail.com>
2026-06-20 19:25:39 +0530

    calcfinal

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,96 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+const evaluateExpression = (expression) => Function(`return (${expression})`)()
+
 function App() {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('')
+
+  const handlebuttonclick = (nextValue) => {
+    if (nextValue === 'AC') {
+      setValue('')
+      return
+    }
+
+    if (nextValue === 'DE') {
+      setValue((currentValue) => currentValue.slice(0, -1))
+      return
+    }
+
+    if (nextValue === '=') {
+      try {
+        setValue(evaluateExpression(value).toString())
+      } catch {
+        setValue('Error')
+      }
+      return
+    }
+
+    setValue((currentValue) => currentValue + nextValue)
+  }
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      const key = event.key
+
+      if ((key >= '0' && key <= '9') || key === '.' || key === '+' || key === '-' || key === '*' || key === '/') {
+        setValue((currentValue) => currentValue + key)
+      } else if (key === 'Enter') {
+        try {
+          setValue((currentValue) => evaluateExpression(currentValue).toString())
+        } catch {
+          setValue('Error')
+        }
+      } else if (key === 'Backspace') {
+        setValue((currentValue) => currentValue.slice(0, -1))
+      } else if (key === 'Escape') {
+        setValue('')
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
+  
   return (
       <div className="container">
           <div className="calculator">
-            <form action="">
+            <form>
               <div className="display">
                 <input type="text" value={value} readOnly />
               </div>
               <div>
-                <input type="button" value="AC" onClick={e => setValue("")} />
-                <input type="button" value="DE" onClick={e => setValue(value.slice(0, -1))} />
-                <input type="button" value="." onClick={e => setValue(value + ".")} />
-                <input type="button" value="/" onClick={e => setValue(value + "/")} />
+                <input type="button" value="AC" onClick={() => handlebuttonclick('AC')} />
+                <input type="button" value="DE" onClick={() => handlebuttonclick('DE')} />
+                <input type="button" value="." onClick={() => handlebuttonclick('.')} />
+                <input type="button" value="/" onClick={() => handlebuttonclick('/')} />
               </div>
               <div>
-                <input type="button" value="7" onClick={e => setValue(value + "7")} />
-                <input type="button" value="8" onClick={e => setValue(value + "8")} />
-                <input type="button" value="9" onClick={e => setValue(value + "9")} />
-                <input type="button" value="*" onClick={e => setValue(value + "*")} />
+                <input type="button" value="7" onClick={() => handlebuttonclick('7')} />
+                <input type="button" value="8" onClick={() => handlebuttonclick('8')} />
+                <input type="button" value="9" onClick={() => handlebuttonclick('9')} />
+                <input type="button" value="*" onClick={() => handlebuttonclick('*')} />
               </div>
               <div>
-                <input type="button" value="4" onClick={e => setValue(value + "4")} />
-                <input type="button" value="5" onClick={e => setValue(value + "5")} />
-                <input type="button" value="6" onClick={e => setValue(value + "6")} />
-                <input type="button" value="+" onClick={e => setValue(value + "+")} />
+                <input type="button" value="4" onClick={() => handlebuttonclick('4')} />
+                <input type="button" value="5" onClick={() => handlebuttonclick('5')} />
+                <input type="button" value="6" onClick={() => handlebuttonclick('6')} />
+                <input type="button" value="+" onClick={() => handlebuttonclick('+')} />
               </div>
               <div>
-                <input type="button" value="1" onClick={e => setValue(value + "1")} />
-                <input type="button" value="2" onClick={e => setValue(value + "2")} />
-                <input type="button" value="3" onClick={e => setValue(value + "3")} />
-                <input type="button" value="-" onClick={e => setValue(value + "-")} />
-              </div>
-              <div>
-                <input type="button" value="00" onClick={e => setValue(value + "00")} />
-                <input type="button" value="0" onClick={e => setValue(value + "0")} />
-                <input type="button" className="equal" value="=" onClick={e => setValue(eval(value))} />
+                <input type="button" value="00" onClick={() => handlebuttonclick('00')} />
+                <input type="button" value="0" onClick={() => handlebuttonclick('0')} />
+                <input type="button" className="equal" value="=" onClick={() => handlebuttonclick('=')} />
               </div>
             </form>
           </div>
-      </div>   
+      </div>
   )
-}
+  }
+
+
 
 export default App
